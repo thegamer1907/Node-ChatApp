fc0945f4b4cb91cf5a2fd572673f69b50e168b8e
Harshit Agarwal <harshitagar1907@gmail.com>
2018-06-29 10:23:18 +0530

    Added Style to the page

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,6 @@
 var socket = io();
 socket.on('connect', function() {
   console.log('Connected to server');
-
-  // socket.emit('createMessage', {
-  //   from: 'user2',
-  //   text: 'hey'
-  // }, function() {
-  //   console.log('Got it');
-  // });
-
 });
 
 socket.on('disconnect', function() {
@@ -33,13 +25,15 @@ socket.on('newLocationMessage', function(msg) {
   jQuery('#messages').append(li);
 });
 
+
 jQuery('#message-form').on('submit', function(e) {
   e.preventDefault();
+  var msgbox = jQuery('[name=message]');
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
+    text: msgbox.val()
   }, function() {
-
+     msgbox.val('');
   })
 });
 
@@ -50,13 +44,17 @@ locationButton.on('click', function(){
     return alert('Geolocation not supported');
   }
 
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
+
   navigator.geolocation.getCurrentPosition(function(position){
     //console.log(position);
+    locationButton.removeAttr('disabled').text('Send Location');
     socket.emit('createLocation', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
   }, function() {
+    locationButton.removeAttr('disabled').text('Send Location');
     alert('Unable to fetch location');
   })
 
